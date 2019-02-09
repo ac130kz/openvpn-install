e2e38c4c39812021a9f12217d379f6fc8b7bc1a8
Mikhail Krassavin <mikhail.krassavin@gmail.com>
2019-02-09 23:45:14 +0600

    Update openvpn-install.sh

--- openvpn-install.sh
+++ openvpn-install.sh
@@ -329,7 +329,8 @@ persist-tun
 status openvpn-status.log
 verb 3
 crl-verify crl.pem
-tun-mtu 1450
+fragment 1300
+mssfix 1300
 txqueuelen 4000" >> /etc/openvpn/server.conf
 	# Enable net.ipv4.ip_forward for the system
 	echo 'net.ipv4.ip_forward=1' > /etc/sysctl.d/30-openvpn-forward.conf
@@ -415,7 +416,8 @@ cipher none
 setenv opt block-outside-dns
 key-direction 1
 verb 3
-tun-mtu 1450" > /etc/openvpn/client-common.txt
+fragment 1300
+mssfix 1300" > /etc/openvpn/client-common.txt
 	# Generates the custom client.ovpn
 	newclient "$CLIENT"
 	echo
